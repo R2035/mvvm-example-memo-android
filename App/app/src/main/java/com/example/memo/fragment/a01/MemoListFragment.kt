09f33e9ef4b57d2b80bb60db2636bcb73d5ab489
R2035 <r2035.develop@gmail.com>
2021-10-17 13:44:33 +0900

    SearchViewのBindingを実装

--- App/app/src/main/java/com/example/memo/fragment/a01/MemoListFragment.kt
+++ App/app/src/main/java/com/example/memo/fragment/a01/MemoListFragment.kt
@@ -2,8 +2,12 @@ package com.example.memo.fragment.a01
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import com.example.memo.R
 import com.example.memo.databinding.FragmentMemoListBinding
 import com.example.memo.fragment.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.stateViewModel
@@ -18,7 +22,34 @@ class MemoListFragment : BaseFragment<MemoListFragmentViewModel, FragmentMemoLis
         return FragmentMemoListBinding.inflate(inflater, root, attachToParent)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_memo_list, menu)
+        val searchView = menu.findItem(R.id.memo_list_menu_item_search).actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                viewModel.onSearchViewQueryTextSubmit(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                viewModel.onSearchViewQueryTextChange(newText)
+                return true
+            }
+        })
+
+        viewModel.searchText.observe(viewLifecycleOwner) {
+            searchView.setQuery(it, false)
+        }
     }
 }
--- App/app/src/main/java/com/example/memo/fragment/a01/MemoListFragmentViewModel.kt
+++ App/app/src/main/java/com/example/memo/fragment/a01/MemoListFragmentViewModel.kt
@@ -1,12 +1,36 @@
 package com.example.memo.fragment.a01
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.memo.core.repository.memo.contract.MemoRepository
 import com.example.memo.fragment.BaseFragmentViewModel
+import kotlinx.coroutines.flow.map
 
 class MemoListFragmentViewModel(
     savedStateHandle: SavedStateHandle,
     private val memoRepository: MemoRepository,
 ) : BaseFragmentViewModel() {
+    val searchText: LiveData<String>
+
     private val _state = savedStateHandle.getStateFlow(MemoListState.initialValue)
+
+    init {
+        searchText = _state
+            .map { it.searchText }
+            .asLiveData(viewModelScope.coroutineContext)
+    }
+
+    fun onSearchViewQueryTextSubmit(query: String?) {
+        _state.value = _state.value.copy(
+            searchText = query ?: ""
+        )
+    }
+
+    fun onSearchViewQueryTextChange(newText: String?) {
+        _state.value = _state.value.copy(
+            searchText = newText ?: ""
+        )
+    }
 }
