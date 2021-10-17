85b1fac6872fe593aa95d001fe5ab22874bdea6a
R2035 <r2035.develop@gmail.com>
2021-10-17 15:10:58 +0900

    OptionsMenuを実装

--- App/app/src/main/java/com/example/memo/fragment/a02/EditingMemoFragment.kt
+++ App/app/src/main/java/com/example/memo/fragment/a02/EditingMemoFragment.kt
@@ -1,7 +1,11 @@
 package com.example.memo.fragment.a02
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.ViewGroup
+import com.example.memo.R
 import com.example.memo.databinding.FragmentEditingMemoBinding
 import com.example.memo.fragment.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.stateViewModel
@@ -14,5 +18,15 @@ class EditingMemoFragment : BaseFragment<EditingMemoViewModel, FragmentEditingMe
 
     override fun inflate(inflater: LayoutInflater, root: ViewGroup?, attachToParent: Boolean): FragmentEditingMemoBinding {
         return FragmentEditingMemoBinding.inflate(inflater, root, attachToParent)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_editing_memo, menu)
     }
 }
