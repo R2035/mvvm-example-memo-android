ce456d139cf8cfee64f2bd8ee62f985ee30feaa3
R2035 <r2035.develop@gmail.com>
2021-10-17 11:26:12 +0900

    MainActivityの配置を修正

--- App/app/src/main/java/com/example/memo/activity/MainActivity.kt
+++ App/app/src/main/java/com/example/memo/activity/MainActivity.kt
@@ -1,8 +1,13 @@
-package com.example.memo
+package com.example.memo.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.memo.R
 
+/**
+ * アプリ起動中は常に表示されるActivity
+ * Activityは他に使用しない
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
